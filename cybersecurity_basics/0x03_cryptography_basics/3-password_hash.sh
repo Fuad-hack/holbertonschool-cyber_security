9e221b81a6f5fdcf92086d164cdcb048b717f832
Fuad-hack <fuadkhanliyev@gmail.com>
2025-10-26 10:08:12 -0400

    Task - 3

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-# Secure password hash (SHA-512 + random salt) using OpenSSL
-
-# Təsadüfi 16 simvolluq salt yaradılır
-salt=$(openssl rand -hex 8)
-
-# Parol və salt birləşdirilir, sonra SHA-512 ilə heşlənir
-echo -n "$1$salt" | openssl dgst -sha512 > 3_hash.txt
+kk#!/bin/bash
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 > 3_hash.txt
