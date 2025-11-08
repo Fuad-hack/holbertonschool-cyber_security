bfc726040d75336cb0ab6482b11420eca4505b81
Fuad-hack <fuadkhanliyev@gmail.com>
2025-11-08 08:14:35 -0500

    Task - 1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -c 'import sys,base64; s=(sys.argv[1][6:] if sys.argv[1].startswith("{xor}") else sys.argv[1]); s+="="*((4-len(s)%4)%4); r=base64.b64decode(s); sys.stdout.buffer.write(bytes(b^0x42 for b in r))' "$1"
+python3 -c "from base64 import b64decode; print(bytes(byte ^ 0x5f for byte in b64decode('$1'.replace('{xor}', ''))).decode('utf-8'))"
